7d3812215f742045f069d07de230f7e9a826236a
Lee <blues0d4@gmail.com>
2017-06-30 14:25:43 +0900

    test

--- CHAPPIE/src/main/java/kr/co/syteam/service/board/BoardService.java
+++ CHAPPIE/src/main/java/kr/co/syteam/service/board/BoardService.java
@@ -1,5 +1,12 @@
 package kr.co.syteam.service.board;
 
-public interface BoardService {
+import java.util.List;
 
+import kr.co.syteam.domain.board.dto.BoardDTO;
+import kr.co.syteam.domain.board.vo.BoardVO;
+
+public interface BoardService {
+	public void boardWrite(BoardDTO boardDTO) throws Exception;
+	public List<BoardVO> boardList() throws Exception;
+	public BoardVO boardView(String board_no) throws Exception;
 }
