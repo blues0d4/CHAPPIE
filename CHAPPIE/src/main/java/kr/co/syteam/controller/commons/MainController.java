a6b6b027d74973632ead66f19a982b04ec14ba2b
Lee <blues0d4@gmail.com>
2017-07-03 16:49:06 +0900

    Main 수정

--- CHAPPIE/src/main/java/kr/co/syteam/controller/commons/MainController.java
+++ CHAPPIE/src/main/java/kr/co/syteam/controller/commons/MainController.java
@@ -1,13 +1,8 @@
 package kr.co.syteam.controller.commons;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.syteam.commons.URIs;
@@ -24,15 +19,9 @@ public class MainController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(URIs.URI_MAIN)
-	public String main(Locale locale, Model model) {
-		logger.info("Welcome CHAPPIE! The client locale is {}.", locale);
+	public String main() {
+		logger.info("Welcome CHAPPIE! This is MainController");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "main";
 	}
